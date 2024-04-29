e7283bb83594f7e29e0a89b6e273cb4da5252322
cacaii03 <daparmaricar3@gmail.com>
2024-04-29 22:13:20 +0800

    added db name

--- db/config.php
+++ db/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'u593341949.dev_balagan');
 define('DB_PASSWORD', '20221557Balagan');
-define('DB_NAME', 'it28-ecommerce');
+define('DB_NAME', 'u593341949_db_balagan');
  
 /* Attempt to connect to MySQL database */
 try{
