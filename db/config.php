be975c661ccb9c20eb3f36b1dfd694b5659134c3
cacaii03 <daparmaricar3@gmail.com>
2024-04-29 22:09:37 +0800

    added password

--- db/config.php
+++ db/config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '20221557Balagan');
 define('DB_NAME', 'it28-ecommerce');
  
 /* Attempt to connect to MySQL database */
